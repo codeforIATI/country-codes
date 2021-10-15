957996e50f99e3086db76e58aa195a53b6eb5c49
Andy Lulham <a.lulham@gmail.com>
2021-10-15 14:22:15 +0100

    Rewrite without splinter

--- scraper.py
+++ scraper.py
@@ -1,9 +1,12 @@
 import os
-import time
 import csv
 
-from splinter import Browser
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import StaleElementReferenceException
 
 # hack to override sqlite database filename
 # see: https://help.morph.io/t/using-python-3-with-morph-scraperwiki-fork/148
@@ -11,71 +14,79 @@ os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
 import scraperwiki
 
 
-def wait_for_page_to_load(browser, url, css):
-    browser.visit('chrome://newtab')
-    print('Waiting for page to load ... ' + url)
-    browser.visit(url)
-    timeout = 60
-    rate = 1
-    timer = 0
-    while True:
-        if timer >= timeout:
-            raise Exception('Timed out.')
-        if browser.is_element_present_by_css(css):
-            break
-        print('Waiting for page to load ... ' + url)
-        time.sleep(rate)
-        timer += rate
-    print('Page has loaded!')
-    return browser.find_by_css(css).first
-
-
 def setup_browser():
     chrome_options = Options()
-    browser = Browser('chrome', headless=True, options=chrome_options)
+    chrome_options.add_argument('--headless')
+    browser = webdriver.Chrome(options=chrome_options)
     return browser
+
+
+def load_page(browser, url):
+    print('Waiting for page to load ... ' + url)
+    browser.delete_all_cookies()
+    browser.get('chrome://newtab')
+    browser.get(url)
+
+
+def find_elements(browser, css):
+    WebDriverWait(browser, 10).until(
+        expected_conditions.presence_of_element_located((By.CSS_SELECTOR, css))
+    )
+    return browser.find_elements(By.CSS_SELECTOR, css)
 
 
 languages = ['fr']
 base_url = 'https://www.iso.org/obp/ui/'
-table_css = 'table[class="grs-grid"]'
 
 browser = setup_browser()
 url = base_url + '#iso:pub:PUB500001:en'
-table = wait_for_page_to_load(browser, url, table_css)
 
-officially_assigned_code_elements_css = 'td[class="grs-status1"]'
-officially_assigned = table.find_by_css(
-    officially_assigned_code_elements_css)
+load_page(browser, url)
+while True:
+    try:
+        exceptionally_reserved_code_elements_css = 'table[class="grs-grid"] td[class="grs-status4"]'
+        exceptionally_reserved = find_elements(
+            browser,
+            exceptionally_reserved_code_elements_css)
+        countries = [{
+            'code': cell.text,
+            'name_en': cell.get_attribute('title').rstrip('*'),
+            'href': '#' + cell.find_element(By.TAG_NAME, 'a').get_attribute('href').split('#')[1],
+            'active': False,
+        } for cell in exceptionally_reserved]
+        break
+    except StaleElementReferenceException:
+        pass
 
-exceptionally_reserved_code_elements_css = 'td[class="grs-status4"]'
-exceptionally_reserved = table.find_by_css(
-    exceptionally_reserved_code_elements_css)
-
-countries = [{
-    'code': cell.text,
-    'name_en': cell['title'].rstrip('*'),
-    'href': '#' + cell.find_by_tag('a')['href'].split('#')[1],
-    'active': False,
-} for cell in exceptionally_reserved]
-
-countries += [{
-    'code': cell.text,
-    'name_en': cell['title'].rstrip('*'),
-    'href': '#' + cell.find_by_tag('a')['href'].split('#')[1],
-    'active': True,
-} for cell in officially_assigned]
+while True:
+    try:
+        officially_assigned_code_elements_css = 'table[class="grs-grid"] td[class="grs-status1"]'
+        officially_assigned = find_elements(
+            browser,
+            officially_assigned_code_elements_css)
+        countries += [{
+            'code': cell.text,
+            'name_en': cell.get_attribute('title').rstrip('*'),
+            'href': '#' + cell.find_element(By.TAG_NAME, 'a').get_attribute('href').split('#')[1],
+            'active': True,
+        } for cell in officially_assigned]
+        break
+    except StaleElementReferenceException:
+        print("Retrying ...")
 
 for country in countries:
-    el = 'div[class="core-view-summary"]'
+    el = 'div[class="core-view-summary"] div[class="core-view-line"] div[class="core-view-field-value"]'
     for language in languages:
         url = base_url + language + '/' + country['href']
-        table = wait_for_page_to_load(browser, url, el)
-        values = table.find_by_css(
-            'div[class="core-view-line"] div[class="core-view-field-value"]')
-        country['name_' + language] = values[2].text.rstrip('*')
-        country['code_3_digit'] = values[4].text
-
+        load_page(browser, url)
+        while True:
+            try:
+                values = find_elements(browser, el)
+                country['name_' + language] = values[2].text.rstrip('*')
+                country['code_3_digit'] = values[4].text
+                break
+            except StaleElementReferenceException:
+                print("Retrying ...")
 
 os.makedirs("output", exist_ok=True)
 with open(os.path.join("output", "country_codes.csv"), 'w') as csv_f:
